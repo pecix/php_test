644368d5295bc44412240c5c3e35113ae1fe95f6
Artur Petrusiewicz <pecixx@gmail.com>
2019-08-30 22:54:17 +0200

    add gitignore

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ const PASSWORD ="xxxxxx";
 const SESSION_TIME = 300;
 
 const DB_HOST = "localhost";
-const DB_LOGIN ="pecix";
-const DB_PASSWORD = "123qwe!@#QWE";
+const DB_LOGIN ="root";
+const DB_PASSWORD = "";
 const DB_TABLE = "test";
